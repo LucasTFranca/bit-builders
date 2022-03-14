c99dc4dca01961196c0d2db35f1ba0ab6f564525
Lucas T Franca <dev.lucasteixeira@gmail.com>
2022-03-13 21:51:35 -0300

    Component Header created

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import './Header.css';
+
+function Header() {
+  return (
+    <div className="header-container">
+      <h2>Palestra e Mesa redonda</h2>
+      <h1>Construindo as melhores aplicações com JavaScript moderno</h1>
+      <div className="event-information-container">
+        <div>
+          <span>
+            Data:
+          </span>
+          12/10/2025
+        </div>
+        <div>
+          <span>
+            Horario:
+          </span>
+          16h
+        </div>
+        <div>
+          <span>
+            Local:
+          </span>
+          Sao Paulo
+        </div>
+        <button type="button">
+          INSCREVA-SE
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
